33365eeba16a968b9725224842b39395384c3c0f
eriveltoSilva <eriveltoclenio@gmail.com>
2024-07-06 12:57:47 +0100

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.example.smarttrafficlight {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.smarttrafficlight to javafx.fxml;
+    exports com.example.smarttrafficlight;
+    exports com.example.smarttrafficlight.controllers;
+    opens com.example.smarttrafficlight.controllers to javafx.fxml;
+}
